3cc3daed3d44a1b7a708242ea643269c702bbbad
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-08-25 01:38:38 +0300

    adding the background images

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import "./App.scss";
 
 import Countdown from "./Components/Countdown/CountDown";
 
+import prettyHills from "./assets/pattern-hills.svg";
+
 function App() {
   return (
     <>
       <Countdown></Countdown>
+      {/* <img src={prettyHills} alt="" hidden /> */}
     </>
   );
 }
