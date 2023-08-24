3cdb7fe8b7cece55ffe6558d15138856402f2bf3
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-08-25 01:02:13 +0300

    Moving the countdown logic to its own component

---
+++ src/Components/Countdown/Countdown.jsx
@@ -0,0 +1,25 @@
+import classes from "./Countdown.module.scss";
+
+import { useState } from "react";
+
+export default function Countdown() {
+  const [countDown, setCountDown] = useState();
+
+  let countDownDate = new Date("Sep 6, 2023 12:00:00").getTime();
+
+  setInterval(() => {
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    setCountDown(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+  }, 1000);
+  return <>{countDown}</>;
+}
