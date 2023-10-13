7df1c910c6770b7884d61d0553b98d61706dded2
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-10-13 21:07:29 +0300

    adding flip animation (TBC)

--- src/Components/Countdown/Number.jsx
+++ src/Components/Countdown/Number.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import classes from "./Number.module.scss";
 
 export default function Number({ number, type }) {
+  const [flip, setFlip] = useState(false);
+  useEffect(() => {
+    setFlip(true);
+
+    setTimeout(() => {
+      setFlip(false);
+    }, 1500);
+  }, [number]);
+
   return (
-    <div className={classes.container}>
-      <div className={classes.numberContainer}>
-        <p className={classes.number}>{number}</p>
+    <div className={`${flip && classes.flip}`}>
+      <div className={`${classes.container} `}>
+        <div className={classes.numberContainer}>
+          <p className={classes.number}>{number}</p>
+        </div>
+        <p className={classes.countdownText}>{type}</p>
       </div>
-      <p className={classes.countdownText}>{type}</p>
     </div>
   );
 }
